67ee5e40396a3b015f11b44500057225588bacb4
Artur896 <delacruzarturo896@gmail.com>
2026-03-21 13:09:52 -0600

    feat(frontend): configuración de Anchor provider y conexión al programa

--- app/src/anchor.js
+++ app/src/anchor.js
@@ -1,17 +1,24 @@
 import { Connection, PublicKey } from "@solana/web3.js";
-import { AnchorProvider, Program } from "@coral-xyz/anchor";
-import idl from "./idl/blinktasks.json";
+import { AnchorProvider, Program, web3 } from "@coral-xyz/anchor";
+import idl from "./idl/blinktasks.json" assert { type: "json" };
+console.log("IDL:", idl);
 
 const programID = new PublicKey("An6HpDp4ypTZB1mKEFzmvXyHSP1oBPf2KeG9J2MkP2my");
 
 const network = "http://127.0.0.1:8899";
 
 export const getProgram = (wallet) => {
-  const connection = new Connection(network, "processed");
-
-  const provider = new AnchorProvider(connection, wallet, {
-    preflightCommitment: "processed",
-  });
+  const connection = new web3.Connection(network, "processed");
+  
+  const provider = new AnchorProvider(
+    connection,
+    {
+      publicKey: wallet.publicKey,
+      signTransaction: wallet.signTransaction,
+      signAllTransactions: wallet.signAllTransactions,
+    },
+    { preflightCommitment: "processed" }
+  );
 
   return new Program(idl, provider);
 };
